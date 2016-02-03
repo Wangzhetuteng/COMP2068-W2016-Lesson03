eb652008ae85bad0657ebeb4bbaad2dc4910c659
Wangzhetuteng <ericdicaprio@outlook.com>
2016-02-03 16:53:36 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 /// <reference path="_reference.ts" />
-var http = require('http');
+var express = require('express');
+var path = require('path');
+var app = express();
 var port = process.env.port || 3000;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello Node!");
+//main route
+app.get('/', function (req, res, next) {
+    res.sendfile(path.join(__dirname, "Public", "Home.html"));
 });
-var port = process.env.port || 3000;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello Node!");
+//route for about
+app.get('/about', function (req, res) {
+    res.sendfile(path.join(__dirname, "Public", "About.html"));
 });
-server.listen(port, function () {
-    console.log("Server Started... Listening on port:" + port);
+//route for contact
+app.get('/contact', function (req, res) {
+    res.sendfile(path.join(__dirname, "Public", "Contact.html"));
+});
+//route for social
+app.get('/social', function (req, res) {
+    res.sendfile(path.join(__dirname, "Public", "Social.html"));
+});
+//route for skills
+app.get('/skills', function (req, res) {
+    res.sendfile(path.join(__dirname, "Public", "Skills.html"));
+});
+//route for services
+app.get('/services', function (req, res) {
+    res.sendfile(path.join(__dirname, "Public", "Services.html"));
+});
+app.listen(port, function () {
+    console.log("App Server started... on port: " + port);
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,23 +1,41 @@
 /// <reference path="_reference.ts" />
-import http = require('http');
+import express = require('express');
+import path = require('path');
+
+var app:express.Express = express();
 
 var port:number = process.env.port || 3000;
 
-var server:http.Server = http.createServer(
-    function(req:http.ServerRequest, res:http.ServerResponse){
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end("Hello Node!");
-});
-/// <reference path="_reference.ts"/>
-import http = require('http');
-
-var port:number = process.env.port || 3000;
-
-var server:http.Server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain'});
-    res.end("Hello Node!");
+//main route
+app.get('/', function (req:express.Request, res:express.Response, next:any) {
+    res.sendfile(path.join(__dirname, "Public", "Home.html"));
 });
 
-server.listen(port, function () {
-    console.log("Server Started... Listening on port:" + port);
+//route for about
+app.get('/about', function(req:express.Request, res:express.Response){
+    res.sendfile(path.join(__dirname, "Public", "About.html"));
+});
+
+//route for contact
+app.get('/contact', function(req:express.Request, res:express.Response){
+    res.sendfile(path.join(__dirname, "Public", "Contact.html"));
+});
+
+//route for social
+app.get('/social', function(req:express.Request, res:express.Response){
+    res.sendfile(path.join(__dirname, "Public", "Social.html"));
+});
+
+//route for skills
+app.get('/skills', function(req:express.Request, res:express.Response){
+    res.sendfile(path.join(__dirname, "Public", "Skills.html"));
+});
+
+//route for services
+app.get('/services', function(req:express.Request, res:express.Response){
+    res.sendfile(path.join(__dirname, "Public", "Services.html"));
+});
+
+app.listen(port, function(){
+    console.log("App Server started... on port: " + port);
 });
